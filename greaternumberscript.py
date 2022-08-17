965409e0b8df91a44fba807f81c4f05d883f5c83
sharmii99 <110819201+sharmii99@users.noreply.github.com>
2022-08-17 12:14:17 +0530

    Add files via upload

---
+++ greaternumberscript.py
@@ -0,0 +1,11 @@
+#Take two numbers from user to compare
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+#compare both the numbers given here
+
+if num1>= num2:
+  if num1 == num2:
+    print ("Both numbers are equal.")
+  else:
+    print (num1)
+else: print(num2)
